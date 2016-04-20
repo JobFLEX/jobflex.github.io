946c0daebae85b7b885fb80de92f44a29a44cec0
John Hamilton <john@isnapapps.com>
2016-04-20 15:51:08 -0400

    Automated commit at 2016-04-20 19:51:08 UTC by middleman-deploy 2.0.0-alpha

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem "middleman-sprockets", "~> 4.0.0.rc"
 gem 'rouge', '~> 1.10.1'
 gem "middleman-livereload", "~> 3.4.3"
 gem 'redcarpet', '~> 3.3.2'
+gem "middleman-deploy", "~> 2.0.0.pre.alpha"
--- config.rb
+++ config.rb
@@ -26,6 +26,14 @@ activate :autoprefixer do |config|
 end
 
 
+activate :deploy do |deploy|
+    deploy.deploy_method = :git
+    deploy.remote   = 'origin'
+    deploy.strategy = :force_push      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+deploy.build_before = true # default: false
+end
+
 
 # Github pages require relative links
 activate :relative_assets
@@ -42,3 +50,4 @@ configure :build do
   # activate :gzipactivate
 
 end
+
